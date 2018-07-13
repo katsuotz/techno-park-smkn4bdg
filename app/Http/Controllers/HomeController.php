8b4e6f25c3c37edfd3ec9a8e664900a415b9d534
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2018-07-13 22:32:11 +0700

    testing artisan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+    	// \Artisan::call('storage:link');
         return view('guest.home.index', [
             'posts' => Post::orderBy('created_at', 'desc')->limit(3)->get(),
             'partners' => Partner::orderBy('updated_at', 'desc')->get(),
