10ae07b340c29e542de0d1e9cbd6f02e7a93dca0
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2018-07-14 19:55:11 +0700

    fix app key using random string

--- config/app.php
+++ config/app.php
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'base64:jXKB4/eIGOmsA540aGCVdxSpLECxWsw2W2l7fTfF5Vg='),
+    'key' => env('APP_KEY', str_random(32)),
 
     'cipher' => 'AES-256-CBC',
 
